7039a36da013481c21f65bd5a808b61ea9e5656a
Steinar H. Gunderson <sesse@debian.org>
2007-01-03 00:02:25 +0100

    Bump the version number, bump the copyright, and update the FAQ.

--- perl/Sesse/pr0n/Common.pm
+++ perl/Sesse/pr0n/Common.pm
@@ -33,7 +33,7 @@ BEGIN {
 		require Sesse::pr0n::Config_local;
 	};
 
-	$VERSION     = "v2.12";
+	$VERSION     = "v2.20";
 	@ISA         = qw(Exporter);
 	@EXPORT      = qw(&error &dberror);
 	%EXPORT_TAGS = qw();
