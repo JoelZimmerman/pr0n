56429e6ffe41a5cd7eff61e7f770f956973f8a42
root <root@pannekake.samfundet.no>
2007-04-27 01:06:47 +0200

    Fix UTF-8 issues in quotes.

--- perl/Sesse/pr0n/Common.pm
+++ perl/Sesse/pr0n/Common.pm
@@ -91,7 +91,7 @@ sub header {
 		$quote = LWP::Simple::get("http://itk.samfundet.no/include/quotes.cli.php");
 		$quote = "Error: Could not fetch quotes." if (!defined($quote));
 	}
-	Sesse::pr0n::Templates::print_template($r, "header", { title => $title, quotes => $quote });
+	Sesse::pr0n::Templates::print_template($r, "header", { title => $title, quotes => Encode::decode_utf8($quote) });
 }
 
 sub footer {
