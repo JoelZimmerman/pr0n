b45980769f1ad2e6f8028e602bac021fac6c01e4
Steinar H. Gunderson <sesse@debian.org>
2007-06-22 23:35:41 +0200

    Revert r147, as planned.

--- perl/Sesse/pr0n/Templates.pm
+++ perl/Sesse/pr0n/Templates.pm
@@ -3,11 +3,6 @@ use strict;
 use warnings;
 
 use Sesse::pr0n::Common qw(error dberror);
-use XML::SAX::Expat;
-use XML::SAX::Writer;
-use XML::TemplateSAX::Buffer;
-use XML::TemplateSAX::Cleaner;
-use XML::TemplateSAX::Handler;
 
 BEGIN {
 	use Exporter ();
@@ -87,37 +82,6 @@ sub print_template {
 	}
 
 	$r->print($text);
-}
-
-sub output_page {
-	my ($r, $page, $params) = @_;
-
-	$r->content_type('text/html; charset=utf-8');
-	$params->{'version'} = $Sesse::pr0n::Common::VERSION;
-
-	# build up the XML chain
-	my $consumer = ModPerlConsumer->new($r);
-	my $writer = XML::SAX::Writer->new(Output => $consumer);
-	my $cleaner = XML::TemplateSAX::Cleaner->new(Handler => $writer);
-	my $filter = XML::TemplateSAX::Handler->new(Handler => $cleaner, Content => $params);
-	my $parser = XML::SAX::Expat->new(Handler => $filter);
-
-	# kick off the parsing
-	$parser->parse_string(fetch_template($r, $page));
-}
-
-# XML consumer for Apache2::Request
-package ModPerlConsumer;
-
-our @ISA = qw( XML::SAX::Writer::ConsumerInterface );
-sub new {
-	my ($base, $r) = @_;
-	return $base->SUPER::new($r);
-}
-
-sub output {
-	my ($self, $text) = @_;
-	$$self->print($text);
 }
 
 1;
