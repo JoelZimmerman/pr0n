25a2e68cdf9eb1773b68c908b6afc9ff8081478b
Steinar H. Gunderson <sesse@debian.org>
2007-07-25 15:16:37 +0200

    Allow more work_mem when upgrading.

--- sql/upgrade-v2.50.sql
+++ sql/upgrade-v2.50.sql
@@ -1,6 +1,8 @@
 --
 -- Upgrades pre-v2.50 databases to 2.50 format -- not many changes, though.
 --
+SET work_mem=131072;
+
 ALTER TABLE exif_info RENAME COLUMN tag TO key;
 CREATE INDEX exif_info_key ON exif_info ( key );
 CLUSTER exif_info_key ON exif_info;
