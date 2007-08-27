dc4da1c16efc1835e9d8b41d473abeeaefcc681b
Steinar H. Gunderson <sesse@debian.org>
2007-08-27 09:49:00 +0200

    Push v2.51 -- too many bugfixes to let the version number stay.

--- perl/Sesse/pr0n/Common.pm
+++ perl/Sesse/pr0n/Common.pm
@@ -35,7 +35,7 @@ BEGIN {
 		require Sesse::pr0n::Config_local;
 	};
 
-	$VERSION     = "v2.50";
+	$VERSION     = "v2.51";
 	@ISA         = qw(Exporter);
 	@EXPORT      = qw(&error &dberror);
 	%EXPORT_TAGS = qw();
