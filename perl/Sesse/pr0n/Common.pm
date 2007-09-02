7ce24b9f75fa54c60737995953b60e96f43f8e8c
Steinar H. Gunderson <sesse@debian.org>
2007-09-02 17:36:29 +0200

    Handle files ending in .NEF, not just .nef.

--- perl/Sesse/pr0n/Common.pm
+++ perl/Sesse/pr0n/Common.pm
@@ -392,7 +392,7 @@ sub ensure_cached {
 		# ImageMagick can handle NEF files, but it does it by calling dcraw as a delegate.
 		# The delegate support is rather broken and causes very odd stuff to happen when
 		# more than one thread does this at the same time. Thus, we simply do it ourselves.
-		if ($filename =~ /\.nef$/) {
+		if ($filename =~ /\.nef$/i) {
 			# this would suffice if ImageMagick gets to fix their handling
 			# $fname = "NEF:$fname";
 			
