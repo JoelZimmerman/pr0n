3beccc23567ed94b627434de774fe50a34b53cba
Steinar H. Gunderson <sesse@debian.org>
2007-09-02 23:08:13 +0200

    Bump to v2.52.

--- perl/Sesse/pr0n/Common.pm
+++ perl/Sesse/pr0n/Common.pm
@@ -35,7 +35,7 @@ BEGIN {
 		require Sesse::pr0n::Config_local;
 	};
 
-	$VERSION     = "v2.51";
+	$VERSION     = "v2.52";
 	@ISA         = qw(Exporter);
 	@EXPORT      = qw(&error &dberror);
 	%EXPORT_TAGS = qw();
