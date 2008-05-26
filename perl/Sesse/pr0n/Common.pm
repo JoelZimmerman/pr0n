e952fa06d546aa37de3c5b72e6e92656b28f232b
Steinar H. Gunderson <sesse@debian.org>
2008-05-27 00:06:25 +0200

    Bump version to v2.65.

--- perl/Sesse/pr0n/Common.pm
+++ perl/Sesse/pr0n/Common.pm
@@ -35,7 +35,7 @@ BEGIN {
 		require Sesse::pr0n::Config_local;
 	};
 
-	$VERSION     = "v2.60";
+	$VERSION     = "v2.65";
 	@ISA         = qw(Exporter);
 	@EXPORT      = qw(&error &dberror);
 	%EXPORT_TAGS = qw();
